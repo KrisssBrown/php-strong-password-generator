59c1e99b9e68b17144668e3c31f21a538b78fadd
KrisssBrown <cristianbruno.cs@gmail.com>
2023-03-22 23:56:24 +0100

    TO FIX password printing

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php  
+<?php
+$pw_num = isset($_GET['pw-lenght']) ? $_GET['pw-lenght'] : 1;
 
-$password= array_merge(range(0,1000), range('a','z'), range('A','Z'));
+$password = array_merge(range('0', '9'), range('a', 'z'), range('A', 'Z'));
 
-$generated_password = [];
+$generated_password =  array_rand($password, intval($pw_num));
 
-$pw_num= $_GET ['pw-lenght'];
+
 
 // var_dump($password)
 
@@ -13,29 +14,38 @@ $pw_num= $_GET ['pw-lenght'];
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
 </head>
+
 <body>
     <form action="" method="GET">
         <p>
             Lunghezza Password
         </p>
         <input type="text" name="pw-lenght" placeholder="Inserisci numero">
-        <input type="submit" value="Genera" >
+        <input type="submit" value="Genera">
 
-        <?php 
-            if ($pw_num > 10) {
+        <?php
+        if ($pw_num >= 6) {
         ?>
             <p>
-                La password è: <?php echo array_rand($password, $pw_num)  ?>
+                La password è: <?php echo implode("", $generated_password)  ?>
             </p>
-        <?php     
-            } 
+        <?php
+        } else {
+        ?>
+            <p>
+                Inserire numero caratteri da stampare
+            </p>
+        <?php
+        }
         ?>
     </form>
 </body>
+
 </html>
